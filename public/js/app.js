6d952c5a6b44d1d74d004e22cb909525149c4eb2
milanbrankovic90 <milanvg90@gmail.com>
2019-04-17 22:44:44 +0200

    Commit

--- public/js/app.js
+++ public/js/app.js
@@ -49427,8 +49427,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/macbookair/www/reserveit/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/macbookair/www/reserveit/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\reserveit\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\reserveit\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
