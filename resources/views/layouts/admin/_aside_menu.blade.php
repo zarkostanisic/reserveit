0ca88fdf8efd91d3805e96300bbd8e4d984013d3
Zarko Stanisic <zarko.stanisic@live.com>
2019-04-24 12:10:59 +0200

    first template setup

---
+++ resources/views/layouts/admin/_aside_menu.blade.php
@@ -0,0 +1,22 @@
+<!-- BEGIN: Aside Menu -->
+<div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " m-menu-vertical="1" m-menu-scrollable="0" m-menu-dropdown-timeout="500">
+	<ul class="m-menu__nav ">
+		<li class="m-menu__section m-menu__section--first">
+			<h4 class="m-menu__section-text">Reservations app</h4>
+			<i class="m-menu__section-icon flaticon-more-v2"></i>
+		</li>
+		<li <?php if(@$meni==1) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true"><a href="index.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-line-graph"></i><span class="m-menu__link-text">Dashboard</span></a></li>
+
+		<li <?php if(@$meni==2) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true" m-menu-link-redirect="1"><a href="objects.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-home-1"></i><span class="m-menu__link-text">Objekti</span></a></li>
+
+		<li <?php if(@$meni==3) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true"><a href="users.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-avatar"></i><span class="m-menu__link-text">Korisnici</span></a></li>
+
+		<li <?php if(@$meni==4) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-signs"></i><span class="m-menu__link-text">Događaji</span></a></li>
+
+		<li <?php if(@$meni==5) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true" m-menu-link-redirect="1"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-calendar"></i><span class="m-menu__link-text">Rezervacije</span></a></li>
+
+		<li <?php if(@$meni==6) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true" m-menu-link-redirect="1"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-settings-1"></i><span class="m-menu__link-text">Podešavanja</span></a></li>
+	</ul>
+</div>
+
+<!-- END: Aside Menu -->
---
+++ resources/views/layouts/admin/admin.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+	<!-- begin::Head -->
+	<head>
+		<meta charset="utf-8" />
+		<title>{{ config('app.name', 'Laravel') }} | Dashboard</title>
+		<meta name="description" content="Latest updates and statistic charts">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+
+		<!--begin::Web font -->
+		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+		<script>
+			WebFont.load({
+            google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+          });
+        </script>
+
+		<!--end::Web font -->
+
+		<!--begin::Global Theme Styles -->
+		<link href="assets/vendors/base/vendors.bundle.css" rel="stylesheet" type="text/css" />
+
+		<!--RTL version:<link href="assets/vendors/base/vendors.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
+		<link href="assets/demo/demo12/base/style.bundle.css" rel="stylesheet" type="text/css" />
+
+		<!--RTL version:<link href="assets/demo/demo12/base/style.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
+
+		<!--end::Global Theme Styles -->
+
+		<!--begin::Page Vendors Styles -->
+		<link href="assets/vendors/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+
+		<!--RTL version:<link href="assets/vendors/custom/fullcalendar/fullcalendar.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
+
+		<!--end::Page Vendors Styles -->
+		<link rel="shortcut icon" href="assets/demo/demo12/media/img/logo/favicon.ico" />
+	</head>
+
+	<!-- end::Head -->
+
+	<!-- begin::Body -->
+	<body class="m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
+
+		<!-- begin:: Page -->
+		<div class="m-grid m-grid--hor m-grid--root m-page">
+
+			@include ('layouts.admin._header')
+
+			<!-- begin::Body -->
+			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
+
+				<!-- BEGIN: Left Aside -->
+				<button class="m-aside-left-close  m-aside-left-close--skin-dark " id="m_aside_left_close_btn"><i class="la la-close"></i></button>
+				<div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-dark ">
+
+					@include ('layouts.admin._aside_menu')
+				</div>
+
+				<!-- END: Left Aside -->
+
+				<div class="m-grid__item m-grid__item--fluid m-wrapper">
+					@include ('layouts.admin._content')
+				</div>
+			</div>
+
+			<!-- end:: Body -->
+
+			@include ('layouts.admin._footer')
+		</div>
+
+		<!-- end:: Page -->
+
+		{{-- @include ('layouts.admin._quick_sidebar') --}}
+
+		<!-- begin::Scroll Top -->
+		<div id="m_scroll_top" class="m-scroll-top">
+			<i class="la la-arrow-up"></i>
+		</div>
+
+		<!-- end::Scroll Top -->
+
+		{{-- @include ('layouts.admin._quick_nav') --}}
+
+		<!--begin::Global Theme Bundle -->
+		<script src="assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
+		<script src="assets/demo/demo12/base/scripts.bundle.js" type="text/javascript"></script>
+
+		<!--end::Global Theme Bundle -->
+
+		<!--begin::Page Vendors -->
+		<script src="assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+
+		<!--end::Page Vendors -->
+
+		<!--begin::Page Scripts -->
+		<script src="assets/app/js/dashboard.js" type="text/javascript"></script>
+
+		<!--end::Page Scripts -->
+	</body>
+
+	<!-- end::Body -->
+</html>
