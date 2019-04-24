fdaff5ac5379ec909bc85f1597f99e4270e6255c
Zarko Stanisic <zarko.stanisic@live.com>
2019-04-24 12:47:51 +0200

    localization for aside menu

---
+++ resources/lang/en/aside_menu.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Aside menu Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines for aside menu in admin panel
+    |
+    */
+
+    'description' => 'Reservation app',
+    'dashboard' => 'Dashboard',
+    'companies' => 'Companies',
+    'users' => 'Users',
+    'events' => 'Events',
+    'reservations' => 'Reservations',
+    'settings' => 'Settings'
+
+];
---
+++ resources/lang/sr/aside_menu.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Aside menu Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines for aside menu in admin panel
+    |
+    */
+
+    'description' => 'Aplikacija za rezervacije',
+    'dashboard' => 'Kontrolna tabla',
+    'companies' => 'Kompanije',
+    'users' => 'Korisnici',
+    'events' => 'Događaji',
+    'reservations' => 'Rezervacije',
+    'settings' => 'Podešavanja	'
+];
