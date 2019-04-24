9309d957df71f112d6a8acc0d1a5d88d85479007
Zarko Stanisic <zarko.stanisic@live.com>
2019-04-24 16:28:10 +0200

    localization finished

--- resources/views/layouts/admin/_locale.blade.php
+++ resources/views/layouts/admin/_locale.blade.php
@@ -1,23 +1,22 @@
 <li class="m-dropdown m-dropdown--inline m-dropdown--arrow m-stack__item--middle" m-dropdown-toggle="click" aria-expanded="true">
 	<a href="#" class="m-dropdown__toggle btn btn-brand dropdown-toggle">
-		{{ strtoupper(str_replace('_', '-', app()->getLocale())) }}													</a>
+		{{ strtoupper(app()->getLocale()) }}													</a>
 	<div class="m-dropdown__wrapper" style="z-index: 101;">
 		<span class="m-dropdown__arrow m-dropdown__arrow--left"></span>
 		<div class="m-dropdown__inner">
 			<div class="m-dropdown__body">
 				<div class="m-dropdown__content">
 					<ul class="m-nav">
-						<li class="m-nav__item">
-							<a href="{{ route('locale', 'en') }}" class="m-nav__link">
-								<span class="m-nav__link-text">EN</span>
-							</a>
-						</li>
 
-						<li class="m-nav__item">
-							<a href="{{ route('locale', 'sr') }}" class="m-nav__link">
-								<span class="m-nav__link-text">SR</span>
-							</a>
-						</li>
+						@foreach ($languages as $lang)
+							@if (app()->getLocale() != $lang)
+								<li class="m-nav__item">
+									<a href="{{ route('locale', $lang) }}" class="m-nav__link">
+										<span class="m-nav__link-text">{{ strtoupper($lang) }}</S></span>
+									</a>
+								</li>
+							@endif
+						@endforeach
 					</ul>
 				</div>
 			</div>
