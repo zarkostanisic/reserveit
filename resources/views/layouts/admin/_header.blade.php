477fa0f7e2ec6f8d6c5183fb3a9a4de0f8ea840b
Zarko Stanisic <zarko.stanisic@live.com>
2019-04-24 19:19:44 +0200

    fixx on mobile

--- resources/views/layouts/admin/_header.blade.php
+++ resources/views/layouts/admin/_header.blade.php
@@ -28,10 +28,10 @@
 						<!-- END -->
 
 						<!-- BEGIN: Responsive Header Menu Toggler -->
-						<a id="m_aside_header_menu_mobile_toggle" href="javascript:;" class="m-brand__icon m-brand__toggler m--visible-tablet-and-mobile-inline-block">
+						{{-- <a id="m_aside_header_menu_mobile_toggle" href="javascript:;" class="m-brand__icon m-brand__toggler m--visible-tablet-and-mobile-inline-block">
 							<span></span>
 						</a>
-
+ --}}
 						<!-- END -->
 
 						<!-- BEGIN: Topbar Toggler -->
