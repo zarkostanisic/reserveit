05f6fb4512d07f4c79380469dd7820b69c492dc0
Zarko Stanisic <zarko.stanisic@live.com>
2019-04-24 15:58:56 +0200

    active links

--- resources/views/layouts/admin/_aside_menu.blade.php
+++ resources/views/layouts/admin/_aside_menu.blade.php
@@ -5,17 +5,17 @@
 			<h4 class="m-menu__section-text">{{ __('aside_menu.description') }}</h4>
 			<i class="m-menu__section-icon flaticon-more-v2"></i>
 		</li>
-		<li <?php if(@$meni==1) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true"><a href="index.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-line-graph"></i><span class="m-menu__link-text">{{ __('aside_menu.dashboard') }}</span></a></li>
+		<li class="m-menu__item  {{ request()->routeIs('dashboard') ? 'm-menu__item--active' : '' }}" aria-haspopup="true"><a href="index.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-line-graph"></i><span class="m-menu__link-text">{{ __('aside_menu.dashboard') }}</span></a></li>
 
-		<li <?php if(@$meni==2) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true" m-menu-link-redirect="1"><a href="objects.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-home-1"></i><span class="m-menu__link-text">{{ __('aside_menu.companies') }}</span></a></li>
+		<li class="m-menu__item  {{ request()->routeIs('companies') ? 'm-menu__item--active' : '' }}" aria-haspopup="true" m-menu-link-redirect="1"><a href="objects.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-home-1"></i><span class="m-menu__link-text">{{ __('aside_menu.companies') }}</span></a></li>
 
-		<li <?php if(@$meni==3) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true"><a href="users.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-avatar"></i><span class="m-menu__link-text">{{ __('aside_menu.users') }}</span></a></li>
+		<li class="m-menu__item  {{ request()->routeIs('users') ? 'm-menu__item--active' : '' }}" aria-haspopup="true"><a href="users.php" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-avatar"></i><span class="m-menu__link-text">{{ __('aside_menu.users') }}</span></a></li>
 
-		<li <?php if(@$meni==4) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-signs"></i><span class="m-menu__link-text">{{ __('aside_menu.events') }}</span></a></li>
+		<li class="m-menu__item  {{ request()->routeIs('events') ? 'm-menu__item--active' : '' }}" aria-haspopup="true"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-signs"></i><span class="m-menu__link-text">{{ __('aside_menu.events') }}</span></a></li>
 
-		<li <?php if(@$meni==5) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true" m-menu-link-redirect="1"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-calendar"></i><span class="m-menu__link-text">{{ __('aside_menu.reservations') }}</span></a></li>
+		<li class="m-menu__item  {{ request()->routeIs('reservations') ? 'm-menu__item--active' : '' }}" aria-haspopup="true" m-menu-link-redirect="1"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-calendar"></i><span class="m-menu__link-text">{{ __('aside_menu.reservations') }}</span></a></li>
 
-		<li <?php if(@$meni==6) {echo 'class="m-menu__item m-menu__item--active"';}else{echo 'class="m-menu__item';}?> aria-haspopup="true" m-menu-link-redirect="1"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-settings-1"></i><span class="m-menu__link-text">{{ __('aside_menu.settings') }}</span></a></li>
+		<li class="m-menu__item  {{ request()->routeIs('settings') ? 'm-menu__item--active' : '' }}" aria-haspopup="true" m-menu-link-redirect="1"><a href="#" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon flaticon-settings-1"></i><span class="m-menu__link-text">{{ __('aside_menu.settings') }}</span></a></li>
 	</ul>
 </div>
 
